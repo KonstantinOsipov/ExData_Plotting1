674108639deda9f1974e3c9face6b775fbd77f13
Blind Pew <49527328+KonstantinOsipov@users.noreply.github.com>
2020-03-23 10:08:33 +0700

    Update Read.data.R

--- Read.data.R
+++ Read.data.R
@@ -1,3 +1,7 @@
 setwd("d:/Work/2019/R")
+#We will use only data from the dates 2007-02-01 and 2007-02-02. 
 dataset <- read.delim('household_power_consumption.txt', header = TRUE, sep = ";", nrows = 120120, colClasses = c('character','character',rep('numeric',7)), na.strings = "?")
 tail(dataset)
+# then let's convert the Date and Time columns to Date/Time classes in R using the strptime() and as.Date() functions.
+dataset$Date <- as.Date(dataset$Date, format = "%d/%m/%Y")
+dataset[((dataset$Date>"2007-02-01") & (dataset$Date<"2007-02-02")) ,]
