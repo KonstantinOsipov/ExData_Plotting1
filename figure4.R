d297e9ad03149a7e47469fac71a599a5524b3354
Blind Pew <49527328+KonstantinOsipov@users.noreply.github.com>
2020-03-27 09:57:25 +0700

    Update figure4.R

--- figure4.R
+++ figure4.R
@@ -4,15 +4,14 @@ DF <- read.csv('data_cut.txt', header = TRUE, colClasses = c('character','charac
 DF$DateTime <- paste(DF$Date, DF$Time)
 DF$DateTime <- as.POSIXct(DF$DateTime)
 #Weekdays is in Russian due to Russian localization.
-#png(file="figure3.png", type = "cairo", bg = "transparent")
+png(file="figure4.png", type = "cairo", bg = "transparent")
 par(mfrow=c(2,2))
 plot(DF$DateTime, DF$Global_active_power, type = 'l', ylab = 'Global active power', xlab = "")
 plot(DF$DateTime, DF$Voltage, type = 'l', ylab = 'Voltage', xlab = "datetime")
-plot(DF$DateTime, DF$Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = "datetime")
 plot(DF$DateTime, DF$Sub_metering_1, type = 'l', ylab = 'Energy sub metering', xlab = "")
 lines(DF$DateTime, DF$Sub_metering_2, type = 'l', col = "red")
 lines(DF$DateTime, DF$Sub_metering_3, type = 'l', col = "blue")
 legend("topright", legend=colnames(DF)[c(7:9)], col=c("blue","red","black"), lty=c(1,1,1))
+plot(DF$DateTime, DF$Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = "datetime")
 dev.off()
-
 
